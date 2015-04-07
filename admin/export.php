b59abc0d4c6477135d3b15347c88475eac678784
prolin99 <you@example.com>
2015-04-07 15:08:32 +0800

    匯出 excel 2007 格式

--- admin/export.php
+++ admin/export.php
@@ -145,11 +145,11 @@ if  ($_GET['mid']) {
 
  	
 	header('Content-Type: application/vnd.ms-excel');
-	header('Content-Disposition: attachment;filename=after'.date("mdHi").'.xls' );
+	header('Content-Disposition: attachment;filename=after'.date("mdHi").'.xlsx' );
 	header('Cache-Control: max-age=0');
 
-	//$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
-	$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+	$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+	//$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
 	$objWriter->save('php://output');
 	exit;		 	
 }	
