2872df9429d19a61ba79496e16b3bf0b197f7030
prolin99 <prolin99@gmail.com>
2015-11-20 10:03:42 +0800

    b3版面

--- function.php
+++ function.php
@@ -139,7 +139,7 @@ function get_sign_kind($id = 0, $mode = 'list', $class_id = 0, $isAdmin = 0)
 
         //if (in_array(1,$xoopsUser->groups())  or  ($mode=='list' )  )		{ //系統管理員，列出全部
             $sql = '  SELECT  *  ,( end_date >=  (NOW() - INTERVAL 1 DAY ) ) as cando , (datediff(`end_date`,now()) +1)  as d_days   FROM '.
-                    $xoopsDB->prefix('sign_kind').' order by   id DESC  LIMIT 0 , 15 ';
+                    $xoopsDB->prefix('sign_kind').' order by  cando DESC,   id DESC  LIMIT 0 , 15 ';
         /*
         }else {
             //列出全部記錄
